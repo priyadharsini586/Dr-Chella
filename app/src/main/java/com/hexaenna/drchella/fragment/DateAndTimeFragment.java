ca9d2fb65115d405bc78d1d6605a9d80596d8224
Priya Mohan <priyadharsini586@gmail.com>
2017-10-28 00:30:36 +0530

    date fixed

--- app/src/main/java/com/hexaenna/drchella/fragment/DateAndTimeFragment.java
+++ app/src/main/java/com/hexaenna/drchella/fragment/DateAndTimeFragment.java
@@ -3,6 +3,7 @@ package com.hexaenna.drchella.fragment;
 import android.annotation.TargetApi;
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.icu.text.SimpleDateFormat;
 import android.icu.util.Calendar;
 import android.net.Uri;
@@ -10,9 +11,11 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -38,6 +41,7 @@ import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 
 import fr.ganfra.materialspinner.MaterialSpinner;
 
@@ -52,6 +56,7 @@ public class DateAndTimeFragment extends Fragment {
     MaterialCalendarView calendarView;
     Calendar cal,c;
     Button btnOk;
+    int currentDate,currentMonth,endMonth,endDate;
     Dialog dialog;
     @TargetApi(Build.VERSION_CODES.N)
     @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
@@ -73,8 +78,15 @@ public class DateAndTimeFragment extends Fragment {
             int thisYear = c.get(Calendar.YEAR);
             int thisMonth = c.get(Calendar.MONTH);
 
+            currentDate = c.get(Calendar.DATE);
+            currentMonth = c.get(Calendar.MONTH);
+
             int end_year = cal.get(Calendar.YEAR);
             int end_month = cal.get(Calendar.MONTH);
+
+            endDate = cal.get(Calendar.DATE);
+            endMonth = cal.get(Calendar.MONTH);
+
 
             calendarView.state().edit()
                     .setMinimumDate(CalendarDay.from(thisYear,thisMonth,1))
@@ -102,10 +114,35 @@ public class DateAndTimeFragment extends Fragment {
                     .commit();
 
         }
-        calendarView.addDecorator(new PriviousDayDisableDecorator());
+        calendarView.addDecorator(new PriviousDayDisableDecorator(getActivity(),currentDate,currentMonth,endDate,endMonth));
+
+
+        Calendar cal1 = Calendar.getInstance();
+        cal1.set(2017, 11, 1);
+        Calendar cal2 = Calendar.getInstance();
+        cal2.set(2017, 11, 10);
+
+        HashSet<CalendarDay> setDays = getCalendarDaysSet(cal1, cal2);
+        int myColor = R.color.colorAccent;
+        calendarView.addDecorator(new BookingDecorator(myColor, setDays));
+
+
         showDailog(getActivity());
 
         return mainView;
+    }
+
+    private HashSet<CalendarDay> getCalendarDaysSet(Calendar cal1, Calendar cal2) {
+        HashSet<CalendarDay> setDays = new HashSet<>();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            while (cal1.getTime().before(cal2.getTime())) {
+                CalendarDay calDay = CalendarDay.from(cal1);
+                setDays.add(calDay);
+                cal1.add(Calendar.DATE, 1);
+            }
+        }
+
+        return setDays;
     }
 
     public void showDailog(Context context) {
@@ -145,20 +182,67 @@ public class DateAndTimeFragment extends Fragment {
 
     private static class PriviousDayDisableDecorator implements DayViewDecorator {
 
-        boolean[] disableState;
+        int currentDate;
+        int currentMonth;
+        int lastDate;
+        int lastMonth;
+        Context context;
+        public PriviousDayDisableDecorator(Context context,int currentDate,int currentMonth,int lastDate,int lastMonth)
+        {
+            this.currentDate = currentDate;
+            this.currentMonth = currentMonth;
+            this.lastDate = lastDate;
+            this.lastMonth = lastMonth;
+            this.context = context;
+        }
+
         @Override
         public boolean shouldDecorate(CalendarDay day) {
 
-            return disableState[day.getDay()];
+            int date = day.getDay();
+            if (currentMonth == day.getMonth())
+               return date <= currentDate;
+            else if (lastMonth == day.getMonth())
+            {
+                return date >= lastDate;
+            }else {
+                return false;
+            }
+
         }
+
 
         @Override
         public void decorate(DayViewFacade view) {
             view.setDaysDisabled(true);
+//            view.setSelectionDrawable(ContextCompat.getDrawable(context,R.drawable.calendar_selected_date));
+
         }
 
 
-
-
     }
+
+    private class BookingDecorator implements DayViewDecorator {
+        private int mColor;
+        private HashSet<CalendarDay> mCalendarDayCollection;
+
+        public BookingDecorator(int color, HashSet<CalendarDay> calendarDayCollection) {
+            mColor = color;
+            mCalendarDayCollection = calendarDayCollection;
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            return mCalendarDayCollection.contains(day);
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.addSpan(new ForegroundColorSpan(mColor));
+            //view.addSpan(new BackgroundColorSpan(Color.BLUE));
+            view.setBackgroundDrawable(ContextCompat.getDrawable(getContext(),R.drawable.calendar_selected_date));
+
+        }
+    }
+
 }
